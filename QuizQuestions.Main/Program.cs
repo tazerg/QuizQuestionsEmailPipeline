6be6380743ff3b004c9a86acf38ae8f06dbedf19
tazerg <tazerg@yandex.ru>
2025-11-20 21:37:29 +0400

    Init solution

---
+++ QuizQuestions.Main/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizQuestions.Main
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+        }
+    }
+}
